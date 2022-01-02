cc2e7a5a74653b82771fd3c52f51734b0154385b
Kinjal Bhattacharyya <bhattacharyyakinjal@gmail.com>
2022-01-02 20:29:32 +0000

    Visualization for Pull request

--- visualization3.py
+++ visualization3.py
@@ -2,11 +2,38 @@ import pandas as pd
 import matplotlib.pyplot as pp
 
 pullR = pd.read_csv('data/PRCount.csv',on_bad_lines='skip')
+
 pullR['URL'] = pullR.url
 pullR['repo']=pullR['base.repo.name']
 pullR['head_repo_size'] = pullR['head.repo.size']
 pullR['base_repo_size']=pullR['base.repo.size']
+pullR['PullR_Size']= ((abs(pullR['head_repo_size'] - pullR['base_repo_size']))/pullR['base_repo_size'])*100
 
-pullR=pullR[['URL','repo','head_repo_size','base_repo_size']]
-pullR.to_csv('data/temp.csv')
-repos = pd.read_csv('data/repo.csv')
+pullR=pullR[['URL','repo','head_repo_size','base_repo_size','PullR_Size']]
+
+pullR_g= pullR.groupby(["repo"])[["PullR_Size"]].mean()
+pullR_g = pullR_g.rename({'PullR_Size': 'size'})
+pullR_g = pullR_g.sort_values(by=['PullR_Size'],ascending=False)
+c=30
+x=[]
+for i in pullR_g.index:
+    x=x+[i]
+    c=c-1
+    if c==0:
+        break
+c=30
+y=[]
+for i in pullR_g.PullR_Size:
+    y=y+[i]
+    c=c-1
+    if c==0:
+        break
+
+
+pp.bar(x,y)
+pp.xticks(rotation=90)
+fig = pp.gcf()
+fig.subplots_adjust(bottom=0.50)
+fig.savefig('PR1.png', dpi=150)
+pp.show()
+
